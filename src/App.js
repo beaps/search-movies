76f3f3327bc23998e10cd497f34800fd55418b62
Beaps <beapsant@gmail.com>
2019-09-30 19:30:08 +0100

    Use OMDb API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component} from 'react';
 
 import { Title } from './components/Title'
 import { SearchForm } from './components/SearchForm'
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Title>Search Movies</Title>
-      <SearchForm />
-    </div>
-  );
+class App extends Component {
+  state = { results: [] }
+
+  handleResults = (results) => {
+    this.setState({ results})
+  }
+
+  renderResults = () => {
+    const { results } = this.state
+    return results.map(movie => {
+      return <p key={movie.imdbID}>{movie.Title}</p>
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Title>Search Movies</Title>
+        <SearchForm onResults={this.handleResults} />
+        {this.state.results.length === 0
+          ? <p>Sin resultados</p>
+          : this.renderResults()
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+const API_KEY = '7ab239d9'
+
 export class SearchForm extends Component {
   state = {
     inputMovie: ''
@@ -11,7 +13,15 @@ export class SearchForm extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault()
-    alert(this.state.inputMovie)
+    const { inputMovie } = this.state
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+      .then(response => response.json())
+      .then(results => {
+        // console.log(results)
+        const { Search, totalResults } = results
+        console.log({ Search, totalResults})
+        this.props.onResults(Search)
+      })
   }
 
   render() {
