3788af3d2b3cf6b8c3ad91d09197020b28e6fb3f
Beaps <beapsant@gmail.com>
2019-10-22 18:47:00 +0100

    Updates

--- precache-manifest.c4d7963adccba0ce1cfed89168c449c6.js
+++ precache-manifest.c4d7963adccba0ce1cfed89168c449c6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "293d0ff319f8081b70ab67af1c076f99",
+    "revision": "c2ebe00a9aed5576a8b1df793032e36e",
     "url": "/search-movies/index.html"
   },
   {
-    "revision": "09b284bf8c0449fd4662",
+    "revision": "9c760061e53bb9ac97c4",
     "url": "/search-movies/static/css/main.76aebb83.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/search-movies/static/js/2.8c27edc6.chunk.js"
   },
   {
-    "revision": "09b284bf8c0449fd4662",
-    "url": "/search-movies/static/js/main.27b30435.chunk.js"
+    "revision": "9c760061e53bb9ac97c4",
+    "url": "/search-movies/static/js/main.6faa6695.chunk.js"
   },
   {
     "revision": "30029f358accbdfe6b84",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/search-movies/precache-manifest.d4183293546cbb5e113373ce06a713e5.js"
+  "/search-movies/precache-manifest.c4d7963adccba0ce1cfed89168c449c6.js"
 );
 
 self.addEventListener('message', (event) => {
