44638a0320d8fd02eeb99e450186db379ef92f0e
Beaps <beapsant@gmail.com>
2019-10-22 18:19:14 +0100

    Updates

---
+++ precache-manifest.d4183293546cbb5e113373ce06a713e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293d0ff319f8081b70ab67af1c076f99",
+    "url": "/search-movies/index.html"
+  },
+  {
+    "revision": "09b284bf8c0449fd4662",
+    "url": "/search-movies/static/css/main.76aebb83.chunk.css"
+  },
+  {
+    "revision": "5ae71f5ae313d0be78a7",
+    "url": "/search-movies/static/js/2.8c27edc6.chunk.js"
+  },
+  {
+    "revision": "09b284bf8c0449fd4662",
+    "url": "/search-movies/static/js/main.27b30435.chunk.js"
+  },
+  {
+    "revision": "30029f358accbdfe6b84",
+    "url": "/search-movies/static/js/runtime-main.d56756d6.js"
+  }
+]);
