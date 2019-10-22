d35c8e5762060e5806bc16ea6dc7540f08b743ce
Beaps <beapsant@gmail.com>
2019-10-22 18:46:28 +0100

    Fixbug: Mixed Content

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -16,7 +16,7 @@ export class SearchForm extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     const { inputMovie } = this.state
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
       .then(response => response.json())
       .then(results => {
         // console.log(results)
